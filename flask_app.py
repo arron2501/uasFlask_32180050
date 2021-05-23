122b12ef34be5cf195f92f396ec2f06bf62ee1c3
Rivaltino Arron <arron2501@gmail.com>
2021-05-23 16:24:17 +0700

    some sidenav and agents styling changes

--- flask_app.py
+++ flask_app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="Home", home_active="text-red-500", agents_active="text-white")
+    return render_template('index.html', title="Home", home_active="text-red-500", homemenu_active="text-white")
+
+@app.route('/agents')
+def agents():
+    return render_template('/agents/index.html', title="Agents", home_active="text-red-500", agents_active="text-white")
 
 if __name__ == '__main__':
     app.run()
