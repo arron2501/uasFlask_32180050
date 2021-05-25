ee98975cab742090a92d8e6dac00df947ab28f89
Rivaltino Arron <arron2501@gmail.com>
2021-05-25 16:19:39 +0700

    add details pages and some small changes

--- flask_app.py
+++ flask_app.py
@@ -9,5 +9,25 @@ def index():
 def agents():
     return render_template('/agents/index.html', title="Agents", home_active="text-red-500", agents_active="text-white")
 
+@app.route('/agent_details')
+def agent_details():
+    return render_template('/agents/details.html', title="Agent Details", home_active="text-red-500", agents_active="text-white")
+
+@app.route('/weapons')
+def weapons():
+   return render_template('/weapons/index.html', title="Weapons", home_active="text-red-500", weapons_active="text-white")
+
+@app.route('/weapon_details')
+def weapon_details():
+   return render_template('/weapons/details.html', title="Weapon Details", home_active="text-red-500", weapons_active="text-white")
+
+@app.route('/maps')
+def maps():
+   return render_template('/maps/index.html', title="Maps", home_active="text-red-500", maps_active="text-white")
+
+@app.route('/map_details')
+def map_details():
+   return render_template('/maps/details.html', title="Map Details", home_active="text-red-500", maps_active="text-white")
+
 if __name__ == '__main__':
     app.run()
