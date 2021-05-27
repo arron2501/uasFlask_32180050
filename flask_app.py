d35f752aa1573b9040102ed530d783528dae89ca
Rivaltino Arron <arron2501@gmail.com>
2021-05-27 15:19:07 +0700

    finished front end side

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,64 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# Auth
+@app.route('/login')
+def login():
+   return render_template('/auth/login.html', title="Sign In", home_menu="hidden", sidenav="hidden", mobile_sidenav="hidden")
+
+@app.route('/register')
+def register():
+   return render_template('/auth/register.html', title="Register", home_menu="hidden", sidenav="hidden", mobile_sidenav="hidden")
+
+@app.route('/register_admin')
+def register_admin():
+   return render_template('/auth/register_admin.html', title="Register Admin", home_menu="hidden", sidenav="hidden", mobile_sidenav="hidden")
+
+
+
+# Admin
+@app.route('/admin')
+def admin():
+   return render_template('/admin/index.html', title="Admin Page", home_menu="hidden", sidenav="hidden", mobile_sidenav="hidden")
+
+# Manage
+@app.route('/manage_agents')
+def manage_agents():
+   return render_template('/admin/agents/index.html', title="Manage Agents", home_menu="hidden", sidenav="hidden", mobile_sidenav="hidden")
+
+@app.route('/manage_weapons')
+def manage_weapons():
+   return render_template('/admin/weapons/index.html', title="Manage Weapons", home_menu="hidden", sidenav="hidden", mobile_sidenav="hidden")
+
+@app.route('/manage_maps')
+def manage_maps():
+   return render_template('/admin/maps/index.html', title="Manage Maps", home_menu="hidden", sidenav="hidden", mobile_sidenav="hidden")
+
+# Add
+@app.route('/add_agent')
+def add_agents():
+   return render_template('/admin/agents/add.html', title="Add Agent", home_menu="hidden", sidenav="hidden", mobile_sidenav="hidden")
+
+@app.route('/add_weapon')
+def add_weapon():
+   return render_template('/admin/weapons/add.html', title="Add Weapon", home_menu="hidden", sidenav="hidden", mobile_sidenav="hidden")
+
+@app.route('/add_map')
+def add_map():
+   return render_template('/admin/maps/add.html', title="Add Map", home_menu="hidden", sidenav="hidden", mobile_sidenav="hidden")
+
+# Edit
+@app.route('/edit_agent')
+def edit_agents():
+   return render_template('/admin/agents/edit.html', title="Edit Agent", home_menu="hidden", sidenav="hidden", mobile_sidenav="hidden")
+
+@app.route('/edit_weapon')
+def edit_weapon():
+   return render_template('/admin/weapons/edit.html', title="Edit Weapon", home_menu="hidden", sidenav="hidden", mobile_sidenav="hidden")
+
+@app.route('/edit_map')
+def edit_map():
+   return render_template('/admin/maps/edit.html', title="Edit Map", home_menu="hidden", sidenav="hidden", mobile_sidenav="hidden")
 
 
 
@@ -33,9 +91,6 @@ def maps():
 @app.route('/map_details')
 def map_details():
    return render_template('/home/maps/details.html', title="Map Details", home_active="text-red-500", maps_active="text-white")
-
-
-
 
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // mode: "jit",
-  purge: [],
+  mode: "jit",
+  purge: ["./templates/**/*.html", "./flask_app.py"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
